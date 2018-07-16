4ff62ff010432786c445652e373438b6ca0e630b
Maxim <legckostupov.maxim@yandex.ru>
2018-07-16 18:31:13 +0300

    second variant server

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
 <?php
-$jsondata = '{
-  "persons_data": [{
-    "firstName": "Иван",
-    "lastName": "Иванов",
-    "address": "г.Москва, ул. Алиева,2",
-    "phoneNumber": "812 123-1234"
-  }, {
-    "firstName": "Пётр",
-    "lastName": "Сидоров",
-    "address": "г.Москва, ул. Мартынова,88",
-    "phoneNumber": "812 385-1589"
-  }, {
-    "firstName": "Василий",
-    "lastName": "Петров",
-    "address": "г.Москва, ул. Фрунзе,64",
-    "phoneNumber": "812 548-6728"
-  }]
-}';
+$jsondata = file_get_contents('data.json') or exit('Не удалось получить данные');
 $data = json_decode($jsondata) or exit('Ошибка декодирования json');
  ?>
 
@@ -45,24 +28,14 @@ $data = json_decode($jsondata) or exit('Ошибка декодирования 
          <td>Адрес</td>
          <td>Номер телефона</td>
        </tr>
-       <tr>
-         <td><?php echo (!empty($data->persons_data[0]->firstName)) ? $data->persons_data[0]->firstName : 'не удалось получить имя'; ?></td>
-         <td><?php echo (!empty($data->persons_data[0]->lastName)) ? $data->persons_data[0]->lastName : 'не удалось получить фамилию'; ?></td>
-         <td><?php echo (!empty($data->persons_data[0]->address)) ? $data->persons_data[0]->address : 'не удалось получить адрес'; ?></td>
-         <td><?php echo (!empty($data->persons_data[0]->phoneNumber)) ? $data->persons_data[0]->phoneNumber : 'не удалось получить телефон'; ?></td>
-       </tr>
-        <tr>
-          <td><?php echo (!empty($data->persons_data[1]->firstName)) ? $data->persons_data[1]->firstName : 'не удалось получить имя'; ?></td>
-          <td><?php echo (!empty($data->persons_data[1]->lastName)) ? $data->persons_data[1]->lastName : 'не удалось получить фамилию'; ?></td>
-          <td><?php echo (!empty($data->persons_data[1]->address)) ? $data->persons_data[1]->address : 'не удалось получить адрес'; ?></td>
-          <td><?php echo (!empty($data->persons_data[1]->phoneNumber)) ? $data->persons_data[1]->phoneNumber : 'не удалось получить телефон'; ?></td>
-       </tr>
-       <tr>
-         <td><?php echo (!empty($data->persons_data[2]->firstName)) ? $data->persons_data[2]->firstName : 'не удалось получить имя'; ?></td>
-         <td><?php echo (!empty($data->persons_data[2]->lastName)) ? $data->persons_data[2]->lastName : 'не удалось получить фамилию'; ?></td>
-         <td><?php echo (!empty($data->persons_data[2]->address)) ? $data->persons_data[2]->address : 'не удалось получить адрес'; ?></td>
-         <td><?php echo (!empty($data->persons_data[2]->phoneNumber)) ? $data->persons_data[2]->phoneNumber : 'не удалось получить телефон'; ?></td>
-       </tr>
+       <?php foreach ($data->persons_data as $dataPerson) { ?>
+         <tr>
+           <td><?php echo (!empty($dataPerson->firstName)) ? $dataPerson->firstName : 'не удалось получить имя'; ?></td>
+           <td><?php echo (!empty($dataPerson->lastName)) ? $dataPerson->lastName : 'не удалось получить фамилию'; ?></td>
+           <td><?php echo (!empty($dataPerson->address)) ? $dataPerson->address : 'не удалось получить адрес'; ?></td>
+           <td><?php echo (!empty($dataPerson->phoneNumber)) ? $dataPerson->phoneNumber : 'не удалось получить телефон'; ?></td>
+         </tr>
+      <?php } ?>
      </table>
    </body>
  </html>
